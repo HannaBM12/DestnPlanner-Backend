260e7bf04fe4483fa36a7d24b91e47fcf30dc489
HannaBM12 <Hannabmulugeta@gmail.com>
2021-05-24 22:28:30 -0400

    before search api

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,8 +19,7 @@ Traveler.destroy_all
 
     traveler1 = Traveler.create(name: 'Hanna', email: 'Hannabmulugeta@gmail.com', age: '25', password: '123')
 
-    url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1506246&pageSize=25&checkOut=2021-07-12&checkIn=2021-07-13&starRatings=3%2C4%2C5&sortOrder=PRICE&locale=en_US&currency=USD")
-
+    url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1506246&pageSize=10&checkOut=2021-07-01&checkIn=2021-06-26&starRatings=3%2C5&sortOrder=PRICE&locale=en_US&currency=USD")
 
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
@@ -41,12 +40,121 @@ Traveler.destroy_all
             address: hotel["address"]["streetAddress"],
             propid: hotel["id"],
             rating: hotel["starRating"],
-            price: rand(100..350),
+            price: hotel["ratePlan"]["price"]["exactCurrent"],
             neighbourhood: hotel["neighbourhood"],
             distance: hotel["landmarks"][1]["distance"]
         )
      end
-        
+
+
+     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1661225&pageSize=15&checkOut=2021-07-21&checkIn=2021-07-15&starRatings=3%2C%205&sortOrder=PRICE&locale=en_US&currency=USD")
+
+     http = Net::HTTP.new(url.host, url.port)
+     http.use_ssl = true
+     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+     
+     request = Net::HTTP::Get.new(url)
+     request["x-rapidapi-key"] = "#{ENV["RAPID_KEY"]}"
+     request["x-rapidapi-host"] = 'hotels4.p.rapidapi.com'
+
+    response = http.request(request)
+    hotels= JSON.parse(response.read_body)["data"]["body"]["searchResults"]["results"]
+
+     hotels.each do|hotel|
+        Hotel.create(
+            name: hotel["name"], 
+            image: hotel["optimizedThumbUrls"]["srpDesktop"], 
+            location: "Washington DC",
+            address: hotel["address"]["streetAddress"],
+            propid: hotel["id"],
+            rating: hotel["starRating"],
+            price: hotel["ratePlan"]["price"]["exactCurrent"],
+            neighbourhood: hotel["neighbourhood"],
+            distance: hotel["landmarks"][1]["distance"]
+        )
+     end
+
+     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1405364&pageSize=10&checkOut=2021-07-21&checkIn=2021-07-15&starRatings=4%2C%205&sortOrder=PRICE&locale=en_US&currency=USD")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-key"] = "#{ENV["RAPID_KEY"]}"
+    request["x-rapidapi-host"] = 'hotels4.p.rapidapi.com'
+
+    response = http.request(request)
+    hotels= JSON.parse(response.read_body)["data"]["body"]["searchResults"]["results"]
+
+     hotels.each do|hotel|
+        Hotel.create(
+            name: hotel["name"], 
+            image: hotel["optimizedThumbUrls"]["srpDesktop"], 
+            location: "Texas",
+            address: hotel["address"]["streetAddress"],
+            propid: hotel["id"],
+            rating: hotel["starRating"],
+            price: hotel["ratePlan"]["price"]["exactCurrent"],
+            neighbourhood: hotel["neighbourhood"],
+            distance: hotel["landmarks"][1]["distance"]
+        )
+     end
+
+     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1481165&pageSize=10&checkOut=2021-07-01&checkIn=2021-06-25&starRatings=4%2C5&sortOrder=PRICE&locale=en_US&currency=USD")
+
+     http = Net::HTTP.new(url.host, url.port)
+     http.use_ssl = true
+     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+     
+     request = Net::HTTP::Get.new(url)
+     request["x-rapidapi-key"] = "#{ENV["RAPID_KEY"]}"
+     request["x-rapidapi-host"] = 'hotels4.p.rapidapi.com'
+
+     response = http.request(request)
+     hotels= JSON.parse(response.read_body)["data"]["body"]["searchResults"]["results"]
+
+     hotels.each do|hotel|
+        Hotel.create(
+            name: hotel["name"], 
+            image: hotel["optimizedThumbUrls"]["srpDesktop"], 
+            location: "Seattle",
+            address: hotel["address"]["streetAddress"],
+            propid: hotel["id"],
+            rating: hotel["starRating"],
+            price: hotel["ratePlan"]["price"]["exactCurrent"],
+            neighbourhood: hotel["neighbourhood"],
+            distance: hotel["landmarks"][1]["distance"]
+        )
+     end
+
+     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1520387&pageSize=10&checkOut=2021-07-21&checkIn=2021-07-15&starRatings=3%2C%205&sortOrder=PRICE&locale=en_US&currency=USD")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-key"] = "#{ENV["RAPID_KEY"]}"
+    request["x-rapidapi-host"] = 'hotels4.p.rapidapi.com'
+
+    response = http.request(request)
+     hotels= JSON.parse(response.read_body)["data"]["body"]["searchResults"]["results"]
+
+     hotels.each do|hotel|
+        Hotel.create(
+            name: hotel["name"], 
+            image: hotel["optimizedThumbUrls"]["srpDesktop"], 
+            location: "Denver",
+            address: hotel["address"]["streetAddress"],
+            propid: hotel["id"],
+            rating: hotel["starRating"],
+            price: hotel["ratePlan"]["price"]["exactCurrent"],
+            neighbourhood: hotel["neighbourhood"],
+            distance: hotel["landmarks"][1]["distance"]
+        )
+     end
+     
     res1 = Reservation.create(check_in: '2021-06-21', check_out: '2021-06-23', no_of_night: 2, no_of_room: 1, cancelation_policy: 'Free', traveler_id: traveler1.id, hotel_id: Hotel.all.sample.id, total: 200.50)
 
     reviews = Review.create([
