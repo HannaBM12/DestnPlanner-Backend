af3e182e0feb0bbf8eefbe17bd9153fda659b8f5
HannaBM12 <Hannabmulugeta@gmail.com>
2021-05-26 11:30:13 -0400

    Before adding Google maps

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,3 +1,7 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+
 class HotelsController < ApplicationController
 
     # skip_before_action :authenticate, only: [:show, :index]
@@ -13,7 +17,22 @@ class HotelsController < ApplicationController
     end
 
     def search
-        byebug
+        hotel_id = params[:propid]
+        url = URI("https://hotels4.p.rapidapi.com/properties/get-details?id="+ hotel_id)
+
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        
+        request = Net::HTTP::Get.new(url)
+        request["x-rapidapi-key"] = '3ad5c48d3dmshf704b5af68d53e9p1f565djsn4c80a4e80613'
+        request["x-rapidapi-host"] = 'hotels4.p.rapidapi.com'
+        
+        response = http.request(request)
+       
+        data = response.read_body
+        render json: data
+        
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   post "/signup", to: "travelers#signup"
   get "/me", to: "travelers#me"
   patch "/me", to: "travelers#update"
+
+  get '/search/:propid', to: "hotels#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
