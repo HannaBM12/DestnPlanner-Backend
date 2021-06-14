80212e16c96c9cbda4cef678d25fe220500cc29d
HannaBM12 <Hannabmulugeta@gmail.com>
2021-06-13 20:07:30 -0400

    Added Readme

--- README.md
+++ README.md
@@ -1,24 +1,51 @@
-# README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Project Name: Destination Planner.  
 
-Things you may want to cover:
+Project Overview: This is my Final Capstone project at Flatiron School. It was designed to help travelers arrange their hotel and tour reservations at their destinations.  I got inspired to build this app from my previous work experience. I used to work in airline industry and assisted passengers with their flight, hotel and tour accommodation. 
 
-* Ruby version
 
-* System dependencies
+To build this app I used rails for the backend and react for the frontend.  In addition I used Rapid API to seed my database and bring real time hotel information in to the App, Mapbox GL to locate each hotel on a map and JWT to authenticate each user.  The backend is currently running on http://localhost:3000/ and the frontend on http://localhost:3006/
 
-* Configuration
 
-* Database creation
+Dependency Files :
+    "@material-ui/core": "^4.11.4",
+    "@material-ui/icons": "^4.11.2",
+    "@testing-library/jest-dom": "^5.11.4",
+    "@testing-library/react": "^11.1.0",
+    "@testing-library/user-event": "^12.1.10",
+    "bootstrap": "^4.6.0",
+    "classnames": "^2.3.1",
+    "react": "^17.0.2",
+    "react-bootstrap": "^1.6.0",
+    "react-dom": "^17.0.2",
+    "react-map-gl": "^6.1.15",
+    "react-router-dom": "^5.2.0",
+    "react-scripts": "4.0.3",
+    "react-scroll": "^1.8.2",
+    "react-swipeable-views": "^0.14.0",
+    "semantic-ui-css": "^2.4.1",
+    "semantic-ui-react": "^2.0.3",
+    "styled-components": "^5.3.0",
+    "web-vitals": "^1.0.1"
 
-* Database initialization
 
-* How to run the test suite
+I used react router to navigate between the pages, and when the user first land into the page, Before login/singup, he can see list of hotel information by going to hotels tab or use the search bar at the home page and search for a hotel at his/her destination.
 
-* Services (job queues, cache servers, search engines, etc.)
+Once the hotel is displayed he can sort by price/star rating, he can also search by hotel name/location.  But to request more information about a hotel he should login/singup. 
 
-* Deployment instructions
+Once inside the traveler can see detail information about the hotel like, room types, nearby airports, distance from the city center, current Covid-19 measures, if pets are allowed or not, check-in/out times, amenities etc... if he decided to book there is an option for him to make reservations by submitting his check-in and out dates, no of rooms and nights.  He can also submit his hotel reviews. 
 
-* ...
+upon reserving a hotel he will be redirected to his reservations summary page where he can see current and past reservations.  There is an option for him to cancel/update his reservations.
+
+From this page he can also add city tour and see tour options at his current city.  He can book a city tour and also cancel his current or previous tours.
+
+
+To style this App I used Sematic UI and Material UI.
+
+This app is currently tested on IOS.
+
+Contact Info:  
+Name: Hanna Mulugeta
+Email: Hannabmulugeta@gmail.com
+Linkedin: https://www.linkedin.com/in/hanna-mulugeta-720685195/ 
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,40 +17,9 @@ Traveler.destroy_all
 
 
 
-    traveler1 = Traveler.create(name: 'Hanna', email: 'Hannabmulugeta@gmail.com', age: '25', password: '123')
+    traveler1 = Traveler.create(name: 'Hanna', email: 'Hanna@gmail.com', age: '25', password: '123456')
 
-    url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1506246&pageSize=9&checkOut=2021-07-01&checkIn=2021-07-10&starRatings=5&sortOrder=PRICE&locale=en_US&currency=USD")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(url)
-    request["x-rapidapi-key"] = "#{ENV["RAPID_KEY"]}"
-    request["x-rapidapi-host"] = 'hotels4.p.rapidapi.com'
-
-    response = http.request(request)
-    hotels= JSON.parse(response.read_body)["data"]["body"]["searchResults"]["results"]
-
-     hotels.each do|hotel|
-        Hotel.create(
-            name: hotel["name"], 
-            image: hotel["optimizedThumbUrls"]["srpDesktop"], 
-            location: "New York",
-            address: hotel["address"]["streetAddress"],
-            latitude: hotel["coordinate"]["lat"],
-            longitude: hotel["coordinate"]["lon"],
-            propid: hotel["id"],
-            rating: hotel["starRating"],
-            guest_reviews: hotel["guestReviews"]["total"],
-            guest_rating: hotel["guestReviews"]["unformattedRating"],
-            price: hotel["ratePlan"]["price"]["exactCurrent"],
-            neighbourhood: hotel["neighbourhood"],
-            distance: hotel["landmarks"][1]["distance"]
-        )
-     end
-
-     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1535616&pageSize=9&checkOut=2021-07-01&checkIn=2021-07-10&starRatings=3%2C4&sortOrder=PRICE&locale=en_US&currency=USD")
+    url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1506246&pageSize=12&checkOut=2021-07-01&checkIn=2021-07-10&starRatings=5&sortOrder=PRICE&locale=en_US&currency=USD")
 
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
@@ -81,6 +50,99 @@ Traveler.destroy_all
         )
      end
 
+     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1535616&pageSize=12&checkOut=2021-07-01&checkIn=2021-07-10&starRatings=4&sortOrder=PRICE&locale=en_US&currency=USD")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-key"] = "#{ENV["RAPID_KEY"]}"
+    request["x-rapidapi-host"] = 'hotels4.p.rapidapi.com'
+
+    response = http.request(request)
+    hotels= JSON.parse(response.read_body)["data"]["body"]["searchResults"]["results"]
+
+     hotels.each do|hotel|
+        Hotel.create(
+            name: hotel["name"], 
+            image: hotel["optimizedThumbUrls"]["srpDesktop"], 
+            location: "New York",
+            address: hotel["address"]["streetAddress"],
+            latitude: hotel["coordinate"]["lat"],
+            longitude: hotel["coordinate"]["lon"],
+            propid: hotel["id"],
+            rating: hotel["starRating"],
+            guest_reviews: hotel["guestReviews"]["total"],
+            guest_rating: hotel["guestReviews"]["unformattedRating"],
+            price: hotel["ratePlan"]["price"]["exactCurrent"],
+            neighbourhood: hotel["neighbourhood"],
+            distance: hotel["landmarks"][1]["distance"]
+        )
+     end
+
+     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1535616&pageSize=12&checkOut=2021-07-01&checkIn=2021-07-10&starRatings=3&sortOrder=PRICE&locale=en_US&currency=USD")
+
+     http = Net::HTTP.new(url.host, url.port)
+     http.use_ssl = true
+     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+ 
+     request = Net::HTTP::Get.new(url)
+     request["x-rapidapi-key"] = "#{ENV["RAPID_KEY"]}"
+     request["x-rapidapi-host"] = 'hotels4.p.rapidapi.com'
+ 
+     response = http.request(request)
+     hotels= JSON.parse(response.read_body)["data"]["body"]["searchResults"]["results"]
+ 
+      hotels.each do|hotel|
+         Hotel.create(
+             name: hotel["name"], 
+             image: hotel["optimizedThumbUrls"]["srpDesktop"], 
+             location: "New York",
+             address: hotel["address"]["streetAddress"],
+             latitude: hotel["coordinate"]["lat"],
+             longitude: hotel["coordinate"]["lon"],
+             propid: hotel["id"],
+             rating: hotel["starRating"],
+             guest_reviews: hotel["guestReviews"]["total"],
+             guest_rating: hotel["guestReviews"]["unformattedRating"],
+             price: hotel["ratePlan"]["price"]["exactCurrent"],
+             neighbourhood: hotel["neighbourhood"],
+             distance: hotel["landmarks"][1]["distance"]
+         )
+      end
+
+
+     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1661225&pageSize=12&checkOut=2021-07-10&checkIn=2021-07-15&starRatings=3&sortOrder=PRICE&locale=en_US&currency=USD")
+
+     http = Net::HTTP.new(url.host, url.port)
+     http.use_ssl = true
+     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+     
+     request = Net::HTTP::Get.new(url)
+     request["x-rapidapi-key"] = "#{ENV["RAPID_KEY"]}"
+     request["x-rapidapi-host"] = 'hotels4.p.rapidapi.com'
+
+    response = http.request(request)
+    hotels= JSON.parse(response.read_body)["data"]["body"]["searchResults"]["results"]
+
+     hotels.each do|hotel|
+        Hotel.create(
+            name: hotel["name"], 
+            image: hotel["optimizedThumbUrls"]["srpDesktop"], 
+            location: "Washington DC",
+            address: hotel["address"]["streetAddress"],
+            latitude: hotel["coordinate"]["lat"],
+            longitude: hotel["coordinate"]["lon"],
+            propid: hotel["id"],
+            rating: hotel["starRating"],
+            guest_reviews: hotel["guestReviews"]["total"],
+            guest_rating: hotel["guestReviews"]["unformattedRating"],
+            price: hotel["ratePlan"]["price"]["exactCurrent"],
+            neighbourhood: hotel["neighbourhood"],
+            distance: hotel["landmarks"][1]["distance"]
+        )
+     end
 
      
 url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1661225&pageSize=6&checkOut=2021-07-10&checkIn=2021-07-15&starRatings=5&sortOrder=PRICE&locale=en_US&currency=USD")
@@ -114,7 +176,8 @@ url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1
         )
      end
 
-     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1661225&pageSize=15&checkOut=2021-07-10&checkIn=2021-07-01&starRatings=3%2C4&sortOrder=STAR_RATING_HIGHEST_FIRST&locale=en_US&currency=USD")
+     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1661225&pageSize=12&checkOut=2021-07-01&checkIn=2021-07-10&starRatings=4&sortOrder=PRICE&locale=en_US&currency=USD")
+
 
      http = Net::HTTP.new(url.host, url.port)
      http.use_ssl = true
@@ -145,7 +208,7 @@ url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1
         )
      end
 
-    url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1405364&pageSize=12&checkOut=2021-07-21&checkIn=2021-07-15&starRatings=5&sortOrder=PRICE&locale=en_US&currency=USD")
+    url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1405364&pageSize=9&checkOut=2021-07-01&checkIn=2021-07-10&starRatings=5&sortOrder=PRICE&locale=en_US&currency=USD")
 
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
@@ -176,7 +239,7 @@ url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1
         )
      end
 
-     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1405364&pageSize=12&checkOut=2021-07-21&checkIn=2021-07-15&starRatings=3%2C4&sortOrder=STAR_RATING_HIGHEST_FIRST&locale=en_US&currency=USD")
+     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1405364&pageSize=6&checkOut=2021-07-01&checkIn=2021-07-10&starRatings=3&sortOrder=PRICE&locale=en_US&currency=USD")
 
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
@@ -238,7 +301,7 @@ url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1
         )
      end
       
-     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1481165&pageSize=9&checkOut=2021-07-10&checkIn=2021-07-01&starRatings=3%2C4&sortOrder=PRICE&locale=en_US&currency=USD")
+     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1481165&pageSize=12&checkOut=2021-07-01&checkIn=2021-07-10&starRatings=4&sortOrder=PRICE&locale=en_US&currency=USD")
 
      http = Net::HTTP.new(url.host, url.port)
      http.use_ssl = true
@@ -300,7 +363,7 @@ url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1
         )
      end
 
-     url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1520387&pageSize=12&checkOut=2021-07-10&checkIn=2021-07-01&starRatings=4%2C3&sortOrder=STAR_RATING_HIGHEST_FIRST&locale=en_US&currency=USD")
+    url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1&destinationId=1520387&pageSize=12&checkOut=2021-07-01&checkIn=2021-07-10&starRatings=4&sortOrder=PRICE&locale=en_US&currency=USD")
 
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
@@ -330,7 +393,8 @@ url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1
             distance: hotel["landmarks"][1]["distance"]
         )
      end
-     
+
+   
     res1 = Reservation.create(check_in: '2021-06-21', check_out: '2021-06-23', no_of_night: 2, no_of_room: 1, cancelation_policy: 'Free', traveler_id: traveler1.id, hotel_id: Hotel.all.sample.id, total: 200.50)
 
     reviews = Review.create([
@@ -355,16 +419,16 @@ url = URI("https://hotels4.p.rapidapi.com/properties/list?adults1=1&pageNumber=1
 
 Tour1 = Tour.create(name: "Statue of Liberty", description: "A universal symbol of freedom, the Statue of Liberty is the most familiar icon and the largest statue in the world.", timage: "https://www.planetware.com/photos-large/US/new-york-statue-of-liberty-2.jpg", location: "New York", tprice: 10, video: 'https://www.youtube.com/embed/kOAS3bmtSAM')
 Tour2 = Tour.create(name: "Smithsonian National Zoological Park", description: "Founded in 1889, its mission is to provide engaging experiences with animals and create and share knowledge to save wildlife and habitats", timage: "https://www.si.edu/sites/default/files/newsdesk/building/nzp.jpg", location: "Washington DC", tprice: 4.99, video: 'https://www.youtube.com/embed/RYBQHl0WjRM' )
-Tour3 = Tour.create(name: "Washington Monument", description: "The Washington Monument is an obelisk within the National Mall in Washington, D.C., built to commemorate George Washington", timage: "https://washington.org/sites/default/files/styles/generic_hero_banner_1440_x_600/public/washington-monument-dc.jpg?h=510cbb6e&itok=d3U8l7b2", location: "Washington Dc", tprice: 6.99,  video: 'https://www.youtube.com/embed/EAt8vA6UpNE')
+Tour3 = Tour.create(name: "Washington Monument", description: "The Washington Monument is an obelisk within the National Mall in Washington, D.C., built to commemorate George Washington", timage: "https://www.trolleytours.com/wp-content/uploads/2016/06/washington-monument.jpg", location: "Washington Dc", tprice: 6.99,  video: 'https://www.youtube.com/embed/EAt8vA6UpNE')
 Tour4 = Tour.create(name: "National Gallery of Art", description: "The National Gallery of Art serves the nation by welcoming all people to explore and experience art, creativity, and our shared humanity", timage: "https://washington-org.s3.amazonaws.com/s3fs-public/jasmine.huang_courtyard-at-the-national-gallery-of-art-east-building_yesmydccool.jpg", location: "Washington Dc", tprice: 4.99,  video: 'https://www.youtube.com/embed/rZWG7eVFVGM')
 Tour5 = Tour.create(name: "Central Park", description: "Central Park is an urban park in New York City located between the Upper West and Upper East Sides of Manhattan. It is the fifth-largest park in the city by area, covering 843 acres", timage: "https://www.planetware.com/photos-large/USNY/new-york-city-central-park-1.jpg", location: "New York", tprice: 3.99, video: 'https://www.youtube.com/embed/9GJD8bhFeaE' )
-Tour6 = Tour.create(name: "Fort Tryon Park", description: "Fort Tryon Park is a public park located in the Hudson Heights and Inwood neighborhoods of the borough of Manhattan in New York City. The 67-acre park is situated on a ridge in Upper Manhattan, close to the Hudson River to the west", timage: "https://3evlzmko7591que7cpvox4ta-wpengine.netdna-ssl.com/wp-content/uploads/2020/09/Fort-Tryon-Park-NYC-copyright-Byelikova-Dreamstime-1600x800-1.jpg", location: "New York", tprice: 6.99,  video: 'https://www.youtube.com/embed/L6YclBK6lH0' )
+Tour6 = Tour.create(name: "Fort Tryon Park", description: "Fort Tryon Park is a public park located in the Hudson Heights and Inwood neighborhoods of the borough of Manhattan in New York City. The 67-acre park is situated on a ridge in Upper Manhattan, close to the Hudson River to the west", timage: "https://st4.depositphotos.com/1031967/22426/i/1600/depositphotos_224262332-stock-photo-scenic-view-fort-tryon-park.jpg", location: "New York", tprice: 6.99,  video: 'https://www.youtube.com/embed/L6YclBK6lH0' )
 Tour7 = Tour.create(name: "KENILWORTH PARK AND AQUATIC GARDEN", description: "Part of Anacostia Park. Located along the east bank of the Anacostia River, this beautiful spot is filled with hiking trails and boardwalks enticing visitors year round.", timage: "http://www.trolleytours.com/wp-content/uploads/2017/09/kenilworth-park.jpg", location: "Washington DC", tprice: 6.99,  video: 'https://www.youtube.com/embed/sPHJ5F4Oxcw')
 Tour8 = Tour.create(name: "THE YARDS PARK", description: "A waterfront park with a boardwalk running along the Anacostia River. Home to well-known ‘outdoor rooms,’ visitors to this park can wander near the dancing fountains at the entrance that welcome visitors to this special space.", timage: "https://ctycms.com/dc-capitol-riverfront/images/ypt1-dancing-fountains.jpg", location: "Washington Dc", tprice: 7.99, video: 'https://www.youtube.com/embed/yOcqjnq7NtY')
 Tour9 = Tour.create(name: "Abraham Lincoln Memorial", description: "The Abraham Lincoln Memorial is the 5th most visited National Park Service site in the country. In fact, over 7 million people visit the memorial annually.", timage: "https://content.tripster.com/travelguide/wp-content/uploads/2019/07/lincoln-memorial-1746358_1920.jpg", location: "Washington Dc", tprice: 7.99, video: 'https://www.youtube.com/embed/TbQk6c79XNI')
-Tour10 = Tour.create(name: "White House", description: "Obviously, the White House ranks high as one of the top Washington DC landmarks you’ve got to see during your trip.", timage: "https://fcw.com/-/media/GIG/EDIT_SHARED/Buildings/WhiteHouse/whitehouse_earlyspring.jpg", location: "Washington Dc", tprice: 7.99, video: 'https://www.youtube.com/embed/U1JCy5rU_3s')
+Tour10 = Tour.create(name: "White House", description: "Obviously, the White House ranks high as one of the top Washington DC landmarks you’ve got to see during your trip.", timage: "https://yt3.ggpht.com/ytc/AAUvwnjZ_JQT0jeXSxrC5sE_2VKvRaosQBQUC5zm50fh=s900-c-k-c0x00ffffff-no-rj", location: "Washington Dc", tprice: 7.99, video: 'https://www.youtube.com/embed/U1JCy5rU_3s')
 Tour11 = Tour.create(name: "Thomas Jefferson Memorial", description: "The Thomas Jefferson Memorial is one of the most iconic Washington DC landmarks. Modeled after the Pantheon in Rom, this outstanding piece of architecture serves as a symbol of liberty.", timage: "https://content.tripster.com/travelguide/wp-content/uploads/2019/07/Jefferson-Memorial-cherry-blossoms-663578_1920.jpg", location: "Washington Dc", tprice: 8.99,  video: 'https://www.youtube.com/embed/oPhDUNN0-Mg')
-Tour12 = Tour.create(name: "United States Capitol", description: "Serving as the home of the Senate and House of Representatives, the U.S. Capitol building is not only architecturally impressive but a symbol of American democracy the world over.", timage: "https://content.tripster.com/travelguide/wp-content/uploads/2019/07/United_States_Capitol_west_front_edit2.jpg", location: "Washington Dc", tprice: 7.99, video: 'https://www.youtube.com/embed/qvgq1awXNzs')
+Tour12 = Tour.create(name: "United States Capitol", description: "Serving as the home of the Senate and House of Representatives, the U.S. Capitol building is not only architecturally impressive but a symbol of American democracy the world over.", timage: "https://afar-production.imgix.net/uploads/images/post_images/images/bc38jVj5NU/original_open-uri20131024-6181-6f55oj?1383832018?ixlib=rails-0.3.0&auto=format%2Ccompress&crop=entropy&fit=crop&h=719&q=80&w=954", location: "Washington Dc", tprice: 7.99, video: 'https://www.youtube.com/embed/qvgq1awXNzs')
 Tour13 = Tour.create(name: "Radio City Music Hall", description: "Stemming from a legendary venue widely known as “The World’s Most Famous Arena,” today MSG includes a mix of renowned venues that span four of the nation’s largest entertainment markets.", timage: "https://www.newyorkbyrail.com/wp-content/uploads/2018/05/Radio-City-Music-Hall-1200x650.jpg", location: "New York", tprice: 10.99, video: 'https://www.youtube.com/embed/z4d0c_jVs04')
 Tour14 = Tour.create(name: "Lincoln Center for the Performing Arts", description: "Learn more about Lincoln Center’s iconic landmarks—including the Revson Fountain, the newly transformed Alice Tully Hall, and the Metropolitan Opera House!Go behind the scenes at venues where a spectacular range of artists have performed, including Luciano Pavarotti, Mikhail Baryshnikov, Leonard Bernstein, Renée Fleming, and Bruce Springsteen.", timage: "https://www.walksofnewyork.com/blog/wp-content/uploads/2012/04/lincoln-center-theater-nyc-night-credit-spirit-of-america-600x400.jpg", location: "New York", tprice: 15.99, video: "https://www.youtube.com/embed/fGj7ww1GRG8" )
 Tour15 = Tour.create(name: "National Theatre DC", description: "SThe National Theatre is located in Washington, D.C., and functions as a venue for live stage productions with seating for 1,676. Despite its name, it is not a governmentally funded national theatre, but operated by a private, non-profit organization", timage: "https://s3.amazonaws.com/ocn-media/6778d14b-42cd-48c0-9ab2-29b48a621a3f.jpg", location: "Washington Dc", tprice: 10.99, video: 'https://www.youtube.com/embed/Kyj_Ah6QOiU')
